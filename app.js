c02b29a92516dfa44cc20d779de447c17f1487cb
Melisa <melisadurgungol@gmail.com>
2024-12-17 18:12:25 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+function checkBoxes(){
+    const triggerBotom=(window.innerHeight / 6)*4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBotom){
+            box.classList.add('show')
+        }
+        else{
+            box.classList.remove('show')
+        }
+    })
+}
